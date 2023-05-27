d9de9b25397f3bfd8f58e474488c71d0cb95c6b2
Дмитрий Прибавкин <Pribavkin.dmitrij@yandex.ru>
2023-05-28 01:43:00 +0500

    Add frontend & gateway CI

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,5 +5,5 @@ import App from './App';
  it('renders without crashing', () => {
    const div = document.createElement('div');
    const root = createRoot(div);
-   root.render(<App />);
+   root.render(<App/>);
  });
